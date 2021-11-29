6b0686d01da8a62a0c568d3d13880d1fd0da6f07
DanieliSanto0s <danielivitoria93taqu@gmail.com>
2021-11-28 23:41:22 -0300

    projeto contains

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CONTAINS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "frase teste para realizar a atividade";
+            string s2 = "tarefa";
+            string s3 = "atividade";
+
+            Console.WriteLine("A frase será falsa por não possuir a palavra TAREFA :{0} ", s1.Contains(s2));
+
+            Console.WriteLine("A frase será verdadeira, pois na frase se apresenta a palavra ATIVIDADE :{0} ", s1.Contains(s3));
+        }
+    }
+}
